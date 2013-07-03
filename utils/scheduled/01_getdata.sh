4a345f04091f59ca2739565c2f6ce000d7b9aadd
Ha Hong <hahong84@gmail.com>
2013-07-03 00:07:22 -0400

    MISC

--- utils/scheduled/01_getdata.sh
+++ utils/scheduled/01_getdata.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 # This script is meant to be called by cronjobmaster.sh
-test "$PROJROOT" || PROJROOT=/home/array/array/
-test "$LOGDIR" || LOGDIR=$PROJROOT/analysis/scheduled/log/
+test "$PROJROOT" || PROJROOT=/home/array/array2/
+test "$LOGDIR" || LOGDIR=$PROJROOT/utils/scheduled/log/
 LOCK=$LOGDIR/01_getdata.sh.lock
+CURRANIMAL=d005_Tito
 
 ###################################################################
 # -- Get the data 
@@ -15,15 +16,16 @@ fi
 touch $LOCK
 
 # 1. Tito
-ssh labuser@dicarlo3 'mv -b data/Tito*.txt data/blackrock_log/; mv -b data/Tito*.* data/blackrock_default/'   # move potentially dislocated files to collect repo dir
-ssh labuser@dicarlo4 'mv -b data/Tito*.txt data/blackrock_log/; mv -b data/Tito*.* data/blackrock_default/'   # move potentially dislocated files to collect repo dir
-rsync -avzuH --exclude='/.snapshot' --remove-source-files labuser@dicarlo3:data/blackrock_default/Tito*.* $PROJROOT/data/d004_Tito/neudat/ 2>&1 | tee -a $LOGDIR/`date +%Y%m%d_%H%M%S`_Tito_neudat.log &
-rsync -avzuH --exclude='/.snapshot' --remove-source-files labuser@dicarlo4:data/blackrock_default/Tito*.* $PROJROOT/data/d004_Tito/neudat_NSP2/ 2>&1 | tee -a $LOGDIR/`date +%Y%m%d_%H%M%S`_Tito_neudat_dicarlo4.log &
-rsync -avzuH --exclude='/.snapshot' --remove-source-files labuser@dicarlo16:Documents/MWorks/Data/Tito*.mwk $PROJROOT/data/d004_Tito/mwk/ 2>&1 | tee -a $LOGDIR/`date +%Y%m%d_%H%M%S`_Tito_mwk.log &
+## -- NOT NEEDED ANYMORE
+## ssh labuser@dicarlo3 'mv -b data/Tito*.txt data/blackrock_log/; mv -b data/Tito*.* data/blackrock_default/'   # move potentially dislocated files to collect repo dir
+## ssh labuser@dicarlo4 'mv -b data/Tito*.txt data/blackrock_log/; mv -b data/Tito*.* data/blackrock_default/'   # move potentially dislocated files to collect repo dir
+rsync -avzuH --exclude='/.snapshot' --remove-source-files labuser@dicarlo3:data/blackrock_default/Tito*.*   $PROJROOT/data/$CURRANIMAL/neudat_NSP1/ 2>&1 | tee -a $LOGDIR/`date +%Y%m%d_%H%M%S`_Tito_neudat_NSP1.log &
+rsync -avzuH --exclude='/.snapshot' --remove-source-files labuser@dicarlo4:data/blackrock_default/Tito*.*   $PROJROOT/data/$CURRANIMAL/neudat_NSP2/ 2>&1 | tee -a $LOGDIR/`date +%Y%m%d_%H%M%S`_Tito_neudat_NSP2.log &
+rsync -avzuH --exclude='/.snapshot' --remove-source-files labuser@dicarlo16:Documents/MWorks/Data/Tito*.mwk $PROJROOT/data/$CURRANIMAL/mwk/         2>&1 | tee -a $LOGDIR/`date +%Y%m%d_%H%M%S`_Tito_mwk.log &
 wait
 
-rsync -avzuH --exclude='/.snapshot' --remove-source-files labuser@dicarlo3:data/blackrock_log/Tito*.* $PROJROOT/data/d004_Tito/log/ 2>&1 | tee -a $LOGDIR/`date +%Y%m%d_%H%M%S`_Tito_log.log &
-rsync -avzuH --exclude='/.snapshot' --remove-source-files --ignore-existing labuser@dicarlo4:data/blackrock_log/Tito*.* $PROJROOT/data/d004_Tito/log/ 2>&1 | tee -a $LOGDIR/`date +%Y%m%d_%H%M%S`_Tito_log_dicarlo4.log &
+rsync -avzuH --exclude='/.snapshot' --remove-source-files labuser@dicarlo3:data/blackrock_log/Tito*.*       $PROJROOT/data/$CURRANIMAL/log_NSP1/    2>&1 | tee -a $LOGDIR/`date +%Y%m%d_%H%M%S`_Tito_log_NSP1.log &
+rsync -avzuH --exclude='/.snapshot' --remove-source-files labuser@dicarlo4:data/blackrock_log/Tito*.*       $PROJROOT/data/$CURRANIMAL/log_NSP2/    2>&1 | tee -a $LOGDIR/`date +%Y%m%d_%H%M%S`_Tito_log_NSP2.log &
 wait
 
-rm -f $LOCK
+rm -f $LOCK`
