8b36664c069ef8e68dd35088a5d67d2b88236b2e
Ha Hong <hahong84@gmail.com>
2013-07-07 16:32:14 -0400

    MISC

--- utils/scheduled/01_getdata.sh
+++ utils/scheduled/01_getdata.sh
@@ -3,7 +3,10 @@
 test "$PROJROOT" || PROJROOT=/home/array/array2/
 test "$LOGDIR" || LOGDIR=$PROJROOT/utils/scheduled/log/
 LOCK=$LOGDIR/01_getdata.sh.lock
-DSTTITO=d005_Tito
+
+HOST_NSP1=dicarlo3
+HOST_NSP2=dicarlo4
+HOST_MWK=dicarlo16
 
 ###################################################################
 # -- Get the data 
@@ -15,17 +18,26 @@ if [ -f $LOCK ]; then
 fi
 touch $LOCK
 
-# -- 1. Tito
-# NOT NEEDED ANYMORE:
-# ssh labuser@dicarlo3 'mv -b data/Tito*.txt data/blackrock_log/; mv -b data/Tito*.* data/blackrock_default/'   # move potentially dislocated files to collect repo dir
-# ssh labuser@dicarlo4 'mv -b data/Tito*.txt data/blackrock_log/; mv -b data/Tito*.* data/blackrock_default/'   # move potentially dislocated files to collect repo dir
-rsync -avzuH --exclude='/.snapshot' --remove-source-files labuser@dicarlo3:data/blackrock_default/Tito*.*   $PROJROOT/data/$DSTTITO/neudat_NSP1/ 2>&1 | tee -a $LOGDIR/`date +%Y%m%d_%H%M%S`_Tito_neudat_NSP1.log &
-rsync -avzuH --exclude='/.snapshot' --remove-source-files labuser@dicarlo4:data/blackrock_default/Tito*.*   $PROJROOT/data/$DSTTITO/neudat_NSP2/ 2>&1 | tee -a $LOGDIR/`date +%Y%m%d_%H%M%S`_Tito_neudat_NSP2.log &
-rsync -avzuH --exclude='/.snapshot' --remove-source-files labuser@dicarlo16:Documents/MWorks/Data/Tito*.mwk $PROJROOT/data/$DSTTITO/mwk/         2>&1 | tee -a $LOGDIR/`date +%Y%m%d_%H%M%S`_Tito_mwk.log &
-wait
+function getall {
+	animal=$1
+	dst=$2
 
-# do not use & to avoid overwritting
-rsync -avzuH --exclude='/.snapshot' --remove-source-files                         labuser@dicarlo3:data/blackrock_log/Tito*.* $PROJROOT/data/$DSTTITO/log/ 2>&1 | tee -a $LOGDIR/`date +%Y%m%d_%H%M%S`_Tito_log_NSP1.log
-rsync -avzuH --exclude='/.snapshot' --remove-source-files --backup --suffix=.NSP2 labuser@dicarlo4:data/blackrock_log/Tito*.* $PROJROOT/data/$DSTTITO/log/ 2>&1 | tee -a $LOGDIR/`date +%Y%m%d_%H%M%S`_Tito_log_NSP2.log
+	# NOT NEEDED ANYMORE:
+	# ssh labuser@dicarlo3 'mv -b data/Tito*.txt data/blackrock_log/; mv -b data/Tito*.* data/blackrock_default/'   # move potentially dislocated files to collect repo dir
+	# ssh labuser@dicarlo4 'mv -b data/Tito*.txt data/blackrock_log/; mv -b data/Tito*.* data/blackrock_default/'   # move potentially dislocated files to collect repo dir
+	rsync -avuH --exclude='/.snapshot' --remove-source-files labuser@$HOST_NSP1:data/blackrock_default/${animal}*.*  $PROJROOT/data/$dst/neudat_NSP1/ 2>&1 | tee -a $LOGDIR/`date +%Y%m%d_%H%M%S`_${animal}_neudat_NSP1.log &
+	rsync -avuH --exclude='/.snapshot' --remove-source-files labuser@$HOST_NSP2:data/blackrock_default/${animal}*.*  $PROJROOT/data/$dst/neudat_NSP2/ 2>&1 | tee -a $LOGDIR/`date +%Y%m%d_%H%M%S`_${animal}_neudat_NSP2.log &
+	rsync -avuH --exclude='/.snapshot' --remove-source-files labuser@$HOST_MWK:Documents/MWorks/Data/${animal}*.mwk  $PROJROOT/data/$dst/mwk/         2>&1 | tee -a $LOGDIR/`date +%Y%m%d_%H%M%S`_${aniaml}_mwk.log &
+	wait
+
+	# do not use "&" to avoid overwritting
+	rsync -avuH --exclude='/.snapshot' --remove-source-files                         labuser@$HOST_NSP1:data/blackrock_log/${animal}*.* $PROJROOT/data/$dst/log/ 2>&1 | tee -a $LOGDIR/`date +%Y%m%d_%H%M%S`_${animal}_log_NSP1.log
+	rsync -avuH --exclude='/.snapshot' --remove-source-files --backup --suffix=.NSP2 labuser@$HOST_NSP2:data/blackrock_log/${animal}*.* $PROJROOT/data/$dst/log/ 2>&1 | tee -a $LOGDIR/`date +%Y%m%d_%H%M%S`_${animal}_log_NSP2.log
+}
+
+# -- 1. Tito
+#getall Tito d003_Tito 
+#getall Tito d004_Tito 
+getall Tito d005_Tito 
 
 rm -f $LOCK
